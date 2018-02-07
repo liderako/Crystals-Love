9939dd70b71b9635e0b7bfd0cdceb16836ddb561
liderako <trotillostudio@gmail.com>
2018-02-07 15:19:27 +0200

    Update Token.sol
    Вернул сгорание так как оно нужно для CrowdSale второго

--- Crystals-Love/Token/Token.sol
+++ Crystals-Love/Token/Token.sol
@@ -4,12 +4,14 @@ import 	"browser/ERC20.sol";
 import 	"browser/Admin.sol";
 
 contract Token is ERC20, Admin {
+	address public 	_crowdSale;
+    bool	public 	_editEnd;
 	uint 	private _freezingTokens;
 	uint    public  _deadlineForToken; // Later named another name 
 
 	event 	FreezingTokens(address admin, uint amount);
 	event 	DefrostingTokens(address admin, uint amount);
-
+    event 	Burn( address indexed from, uint value );
 	/* 
 	*	"NameToken","SSS","42000000","18", "5"
 	*	construct for remix solidity
@@ -18,7 +20,15 @@ contract Token is ERC20, Admin {
 				ERC20(nameToken, symbolToken, supply, decimals) Admin(msg.sender) public {
 		 _deadlineForToken = now + time * 1 minutes;
 	}
-	
+	/* + */
+    function 	setAddressCrowdSale( address smartContract ) public returns ( bool ) {
+    	assertAdmin();
+        require( _editEnd == false );
+
+        _crowdSale = smartContract;
+        _editEnd = true;
+        return 	true;
+    }
 	/* - */	
 	function 	freezingTokens(uint amount)  public returns (bool) {
 		assertAdmin();
@@ -49,6 +59,21 @@ contract Token is ERC20, Admin {
 
 		DefrostingTokens(getAdmin(), amount);
 		return 	true;
+	}
+	
+	/*
+	**	This is function need for burn tokens crowdSale.
+    	** 	@param uint amount tokens for burn.
+	*/
+	function 	burn( uint amount ) public returns ( bool ) {
+        	require( _balanceOf[msg.sender] >= amount );
+        	require( msg.sender == _crowdSale );
+
+        	_balanceOf[msg.sender] -= amount;
+		_totalSupply -= amount;
+		
+		Burn( msg.sender, amount );
+		return true;
 	}
 	/* + */
 	function    getFreezingTokens() public constant returns ( uint amount ) {

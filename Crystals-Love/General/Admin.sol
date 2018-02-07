ab6da7d5f9e099287030a89f798e50950a1eb55f
liderako <trotillostudio@gmail.com>
2018-02-07 13:31:27 +0200

    Fix bug and deleted function assertNull

--- Crystals-Love/General/Admin.sol
+++ Crystals-Love/General/Admin.sol
@@ -3,20 +3,15 @@ pragma solidity ^0.4.18;
 contract Admin {
 
 	address private _admin;
-	/* - */
+	/* + */
 	function 	Admin( address admin ) public {
-		assertNULL();
+	    if ( admin == address( 0x0 ) )
+	        require( false );
 		_admin = admin;
 	}
 	/* + */
 	function 	assertAdmin() internal view returns ( bool ) {
 		if ( _admin != msg.sender )
-			require( false );
-		return true;
-	}
-	/* - */
-	function 	assertNULL() internal view returns ( bool ){
-		if ( admin == address( 0x0 ) )
 			require( false );
 		return true;
 	}

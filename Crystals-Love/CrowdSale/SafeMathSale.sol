dc120e2587df30efcf3fb498e576ce92a1ecbd6e
liderako <trotillostudio@gmail.com>
2018-02-07 16:44:32 +0200

    Update SafeMathSale.sol

--- Crystals-Love/CrowdSale/SafeMathSale.sol
+++ Crystals-Love/CrowdSale/SafeMathSale.sol
@@ -1,24 +1,28 @@
 pragma solidity ^0.4.18;
 
-contract SafeMathSale {
+library SafeMathSale {
 
-	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-		if (a == 0) {
+	function mul( uint a, uint b ) internal pure returns ( uint ) {
+		uint c;
+
+		if ( a == 0 ) {
 			return 0;
 		}
-		uint256 c = a * b;
-		assert(c / a == b);
+		c = a * b;
+		assert( c / a == b );
 		return c;
 	}
 
-	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    	assert(b <= a);
-    	return a - b;
+	function sub( uint a, uint b ) internal pure returns ( uint ) {
+    		assert( b <= a );
+    		return a - b;
   	}
 
-	function add(uint256 a, uint256 b) internal pure returns (uint256) {
-		uint256 c = a + b;
-		assert(c >= a);
+	function add( uint a, uint b ) internal pure returns ( uint ) {
+		uint c;
+
+		c = a + b;
+		assert( c >= a );
 		return c;
 	}
 

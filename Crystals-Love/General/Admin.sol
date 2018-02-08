8f2f4911c0ae54026d92ef0fa5b1a9433adb0bd8
liderako <trotillostudio@gmail.com>
2018-02-08 16:44:08 +0200

    Update Admin.sol
    delete coment

--- Crystals-Love/General/Admin.sol
+++ Crystals-Love/General/Admin.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.4.18;
 contract Admin {
 
 	address private _admin;
-	/* + */
+
 	function 	Admin( address admin ) public {
 	    if ( admin == address( 0x0 ) )
 	        require( false );
 		_admin = admin;
 	}
-	/* + */
+
 	function 	assertAdmin() internal view returns ( bool ) {
 		if ( _admin != msg.sender )
 			require( false );
 		return true;
 	}
-	/* + */
+
 	function 	getAdmin() public constant returns( address admin ) {
 		return 	_admin;
 	}

c51562aa03b3ec20d676d9c2108577b7d23d5e2c
asvirido <trotillostudio@gmail.com>
2018-04-06 12:01:59 +0300

    betka

--- Business-smart-contract/BetaVersion.sol
+++ Business-smart-contract/BetaVersion.sol
@@ -20,17 +20,106 @@ contract SafeMath {
 }
 
 contract Crystals is SafeMath {
-	mapping(address => uint) public _balanceOf;
-
-
-	// доп функционал на потом
-	// /* 	Метод для того чтобы завести токены на замороженое состояние исполнителя из внешнего баланса. */
-	function 	depositTokensForExecutor(int amount) public {
-		
+	struct Strongbox{
+		address executor;
+//		address user;
+		uint 	amount;		
 	}
 
-	 // Метод для того чтобы добавить токенов на замороженом состоянии для исполнителя из внешнего баланса
-	function 	additionalDepositTokensForExecutor(int amount) public {
+	mapping(address => uint) public _balanceOf;
+	mapping(bytes32 => mapping(address => Strongbox)) public _strongBoxList;
+	
+    address 					private	_addressTokenSmartContract;
+	
+	function Crystals(address token) public {
+		_addressTokenSmartContract = token;
+	}
+			
+	function 	getAddressTokenSmartContract() public constant returns (address) {
+		return _addressTokenSmartContract;
+	}
+	// функционал под ERC827
+	// // /* 	Метод для того чтобы завести токены на замороженое состояние исполнителя из внешнего баланса. */
+	// function 	depositTokensForExecutor(int amount) public {
 		
+	// }
+
+	//  // Метод для того чтобы добавить токенов на замороженом состоянии для исполнителя из внешнего баланса
+	// function 	additionalDepositTokensForExecutor(int amount) public {
+		
+	// }
+
+	// функционал под ERC20
+	/* Функция для того чтобы завести деньги на личный баланс в смарт-контракте*/
+	function 	depositTokensInWallet(uint amount) public {
+		require (amount != 0);
+		_balanceOf[msg.sender] = safeAdd(_balanceOf[msg.sender], amount);
+		if (Token(getAddressTokenSmartContract()).transferFrom(msg.sender, this, amount) == false) {
+			revert();
+		}
+	}
+
+	/* Функция для того чтобы вывести деньги с личного баланса обратно на адрес msg.sender*/
+	function 	withdrawTokensFromWallet(uint amount) public {
+		require (amount != 0);
+		_balanceOf[msg.sender] = safeSub(_balanceOf[msg.sender], amount);
+		if (Token(getAddressTokenSmartContract()).transfer(msg.sender, amount) == false) {
+			revert();
+		}
+	}
+
+	function 	createOrder(address executor, uint amount) public {
+		require (amount > 0);
+		require (executor != (0x0));
+
+		bytes32 hash = sha256(this, executor, msg.sender);
+		require(getAmount(hash) == 0);
+		setStrongBox(hash, executor, amount);
+		_balanceOf[msg.sender] = safeSub(_balanceOf[msg.sender], amount);
+	}
+
+	function 	addTokensForOrder(address executor, uint amountAdd) public {
+		require (amountAdd > 0);
+		require (executor != (0x0));
+
+		bytes32 hash = sha256(this, executor, msg.sender);
+		uint amountOld = getAmount(hash);
+	    require(amountOld > 0);
+		setStrongBox(hash, executor, safeAdd(amountOld, amountAdd));
+		_balanceOf[msg.sender] = safeSub(_balanceOf[msg.sender], amountAdd);
+	}
+
+	function 	cancelOrder(address executor) public {
+		require (executor != (0x0));
+
+		bytes32 hash = sha256(this, executor, msg.sender);
+		uint amount = getAmount(hash);
+		require(amount != 0);
+		setStrongBox(hash, executor, 0);
+		_balanceOf[msg.sender] = safeAdd(_balanceOf[msg.sender], amount);
+	}
+
+	function 	sendTokensForExecutor(address executor) public {
+		require(executor != (0x0));
+
+		bytes32 hash = sha256(this, executor, msg.sender);
+		uint amount = getAmount(hash);
+		require (amount != 0);
+		setStrongBox(hash, executor, 0);
+		_balanceOf[executor] = safeAdd(_balanceOf[executor], amount);
+	}
+	
+
+// Strongbox
+	function 	getExecutor(bytes32 hash) public constant returns (address) {
+		return _strongBoxList[hash][msg.sender].executor;
+	}
+	function 	getAmount(bytes32 hash) public constant returns (uint) {
+		return _strongBoxList[hash][msg.sender].amount;
+	}
+
+	function 	setStrongBox(bytes32 hash, address executor, uint amount) private {
+		_strongBoxList[hash][msg.sender].executor = executor;
+		_strongBoxList[hash][msg.sender].amount = amount;
 	}
 }
